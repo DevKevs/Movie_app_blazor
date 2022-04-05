c3dc0ba6f6c9e0e317f4e283c17d61a3d1d606b1
DevKevs <kevinfeliz2016@gmail.com>
2022-04-05 14:46:57 -0400

    App v1.3

--- Movie_app/Client/obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
+++ Movie_app/Client/obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
@@ -13,84 +13,84 @@ namespace Movie_app.Client.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "D:\Kevin Backup\repos\Movie_app\Movie_app\Client\_Imports.razor"
+#line 1 "C:\Users\Kevin Feliz\Desktop\Repo\Movie_app_blazor\Movie_app\Client\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "D:\Kevin Backup\repos\Movie_app\Movie_app\Client\_Imports.razor"
+#line 2 "C:\Users\Kevin Feliz\Desktop\Repo\Movie_app_blazor\Movie_app\Client\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "D:\Kevin Backup\repos\Movie_app\Movie_app\Client\_Imports.razor"
+#line 3 "C:\Users\Kevin Feliz\Desktop\Repo\Movie_app_blazor\Movie_app\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "D:\Kevin Backup\repos\Movie_app\Movie_app\Client\_Imports.razor"
+#line 4 "C:\Users\Kevin Feliz\Desktop\Repo\Movie_app_blazor\Movie_app\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "D:\Kevin Backup\repos\Movie_app\Movie_app\Client\_Imports.razor"
+#line 5 "C:\Users\Kevin Feliz\Desktop\Repo\Movie_app_blazor\Movie_app\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "D:\Kevin Backup\repos\Movie_app\Movie_app\Client\_Imports.razor"
+#line 6 "C:\Users\Kevin Feliz\Desktop\Repo\Movie_app_blazor\Movie_app\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "D:\Kevin Backup\repos\Movie_app\Movie_app\Client\_Imports.razor"
+#line 7 "C:\Users\Kevin Feliz\Desktop\Repo\Movie_app_blazor\Movie_app\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "D:\Kevin Backup\repos\Movie_app\Movie_app\Client\_Imports.razor"
+#line 8 "C:\Users\Kevin Feliz\Desktop\Repo\Movie_app_blazor\Movie_app\Client\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "D:\Kevin Backup\repos\Movie_app\Movie_app\Client\_Imports.razor"
+#line 9 "C:\Users\Kevin Feliz\Desktop\Repo\Movie_app_blazor\Movie_app\Client\_Imports.razor"
 using Movie_app.Client;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "D:\Kevin Backup\repos\Movie_app\Movie_app\Client\_Imports.razor"
+#line 10 "C:\Users\Kevin Feliz\Desktop\Repo\Movie_app_blazor\Movie_app\Client\_Imports.razor"
 using Movie_app.Client.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "D:\Kevin Backup\repos\Movie_app\Movie_app\Client\_Imports.razor"
+#line 11 "C:\Users\Kevin Feliz\Desktop\Repo\Movie_app_blazor\Movie_app\Client\_Imports.razor"
 using Radzen;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "D:\Kevin Backup\repos\Movie_app\Movie_app\Client\_Imports.razor"
+#line 12 "C:\Users\Kevin Feliz\Desktop\Repo\Movie_app_blazor\Movie_app\Client\_Imports.razor"
 using Radzen.Blazor;
 
 #line default
@@ -104,7 +104,7 @@ using Radzen.Blazor;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 33 "D:\Kevin Backup\repos\Movie_app\Movie_app\Client\Shared\NavMenu.razor"
+#line 33 "C:\Users\Kevin Feliz\Desktop\Repo\Movie_app_blazor\Movie_app\Client\Shared\NavMenu.razor"
        
     private bool collapseNavMenu = true;
 
